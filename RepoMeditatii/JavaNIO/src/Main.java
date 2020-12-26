a28d76e84798ed650e8e592d0a4f85376e48a292
andrei.baltat <andrei.baltat@adswizz.com>
2020-12-26 15:53:47 +0200

    add java stuff
    add java stuff

--- RepoMeditatii/JavaNIO/src/Main.java
+++ RepoMeditatii/JavaNIO/src/Main.java
@@ -1,5 +1,15 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            FileInputStream file = new FileInputStream("data.txt");
+            FileChannel channel = file.getChannel();
+            
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
