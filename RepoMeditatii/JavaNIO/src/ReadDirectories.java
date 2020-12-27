4da7f4287c5706343c346fb59cee650cf8e623ab
andrei.baltat <andrei.baltat@adswizz.com>
2020-12-27 16:14:51 +0200

    threads
    threads

--- RepoMeditatii/JavaNIO/src/ReadDirectories.java
+++ RepoMeditatii/JavaNIO/src/ReadDirectories.java
@@ -1,5 +1,59 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ReadDirectories {
     public static void main(String[] args) {
 
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>() {
+//                    public boolean accept(Path path) throws IOException {
+//                        return Files.isRegularFile(path);
+//                    }
+//                };
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
+
+//        Path directory = FileSystems.getDefault().getPath("Examples/Dir2");
+        Path directory = FileSystems.getDefault().getPath("Examples" + File.separator + "Dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+
+        try {
+            Path tempFile = Files.createTempFile("myapp",".appext");
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("--Walking tree for Dir2--");
+        Path dir2Path = FileSystems.getDefault().getPath("Examples" + File.separator + "Dir2");
+        try {
+            Files.walkFileTree(dir2Path, new PrintNames());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("---Copy Dir2 to Dir4/Dir2Copy---");
+        Path copyPath = FileSystems.getDefault()
+                .getPath("Examples" + File.separator + "Dir4" + File.separator + "Dir2Copy");
+
+        try {
+            Files.walkFileTree(dir2Path, new CopyFiles(dir2Path, copyPath));
+        } catch (IOException e){
+            e.getMessage();
+        }
     }
 }
